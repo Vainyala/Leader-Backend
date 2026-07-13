98484ccee85bb5344691df5bde4075f5f0517a85
Vainyala17 <samalvainyala174@gmail.com>
2026-07-13 15:40:06 +0530

    admin login api

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
-// models/User.js
+// // models/User.js
+
 const {getISTDate} = require('../utils/helperFunctions'); // Get IST Date function
 
 const mongoose = require('mongoose');
@@ -19,6 +20,7 @@ const userSchema = new mongoose.Schema({
   instagram: String,
   password: String,
   email_otp: String,
+  user_type: { type: String, required: true, Enum: ['admin', 'user'], Default: 'user' },
   isEmailVerified: { type: Boolean, default: false },
   isEmailOTPVerified: { type: Boolean, default: false },
   fcm_token: {type: String,default: null},
@@ -33,3 +35,44 @@ userSchema.index({ leader_regd_mobile_no: 1, user_email_id: 1 }, { unique: true 
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
+
+
+
+
+
+
+
+// const {getISTDate} = require('../utils/helperFunctions'); // Get IST Date function
+
+// const mongoose = require('mongoose');
+
+// const userSchema = new mongoose.Schema({
+//   leader_regd_mobile_no: { type: String, required: true },
+//   user_email_id: { type: String, required: true },
+//   name: String,
+//   mobile: String,
+//   profile_image: String,
+//   address: String,
+//   city: String,
+//   district: String,
+//   state: String,
+//   pincode: String,
+//   facebook: String,
+//   twitter: String,
+//   instagram: String,
+//   password: String,
+//   email_otp: String,
+//   isEmailVerified: { type: Boolean, default: false },
+//   isEmailOTPVerified: { type: Boolean, default: false },
+//   fcm_token: {type: String,default: null},
+// }, 
+// {
+//   timestamps: { currentTime: getISTDate }
+// });
+
+// // Compound unique index: email must be unique per leader
+// userSchema.index({ leader_regd_mobile_no: 1, user_email_id: 1 }, { unique: true });
+
+// const User = mongoose.model('User', userSchema);
+
+// module.exports = User;
